a7c16a851d1392ebe2d38360e8564bc436e5a33f
Oleksandr Domanskiy <domanskyioleksandr@gmail.com>
2025-09-16 23:40:15 +0300

    added CardPage, working logic and styles

--- src/app/components/article-page/article-card/article-card.component.ts
+++ src/app/components/article-page/article-card/article-card.component.ts
@@ -1,15 +1,34 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Article, ArticleService } from '../../../services/article-service.service';
 
 @Component({
   selector: 'app-article-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, RouterModule, MatCardModule, MatButtonModule],
   templateUrl: './article-card.component.html',
   styleUrl: './article-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ArticleCardComponent {
+  article$!: Observable<Article>;
 
+  constructor(
+    private route: ActivatedRoute,
+    private articleService: ArticleService
+  ) { }
+  
+  ngOnInit() {
+    this.article$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = Number(params.get('id'));
+        return this.articleService.getArticleById(id);
+      })
+    );
+  }
 }
